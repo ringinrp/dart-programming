4aa9ce697751a255ecb83fceb474857c27c15ed1
Ringin Restu Pati <ringinrp21@gmail.com>
2023-03-16 16:44:11 +0700

    set

---
+++ set.dart
@@ -0,0 +1,12 @@
+//set tidak ada index dan tidak bisa di deklarasi ulang
+
+void main() {
+  var names = {'Ringin', 'Restu', 'Pati'};
+
+  print(names);
+
+  names.remove('Restu');
+  print(names);
+
+  print(names.length);
+}
