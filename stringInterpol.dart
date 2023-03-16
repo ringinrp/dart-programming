1b7640e3d5d1c57de5dcb7aef8ad1f4fb28212b2
Ringin Restu Pati <ringinrp21@gmail.com>
2023-03-16 08:37:56 +0700

    string

---
+++ stringInterpol.dart
@@ -0,0 +1,10 @@
+void main() {
+  String firstName = 'ringin';
+  String lastName = 'restu pati';
+
+  print(firstName);
+  print(lastName);
+
+  var fullName = '$firstName $lastName';
+  print(fullName);
+}
